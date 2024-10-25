706f4cf2c1988297fb4681dac2a8f783463de5c5
vety01febriyanti <vety.febriyanti208@gmail.com>
2024-10-25 22:53:33 +0700

    1

---
+++ tests/UI/saucedemo-po.spec.js
@@ -0,0 +1,36 @@
+const { test } = require ("@playwright/test");
+const { LoginPage } = require ("./page object/login-page");
+const { DashboardPage } = require("./page object/dashboard-page");
+const { CartPage } = require("./page object/cart-page");
+
+test ('TC 2 - Successful login using page object', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const dashboardPage = new DashboardPage(page)
+
+    await loginPage.navigate()
+    await loginPage.login ('standard_user', 'secret_sauce')
+    await dashboardPage.validateOnPage ()
+
+});
+
+test ('TC 3 - Successful login using page object -visual user', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const dashboardPage = new DashboardPage(page)
+
+    await loginPage.navigate()
+    await loginPage.login ('visual_user', 'secret_sauce')
+    await dashboardPage.validateOnPage ()
+    
+});
+
+test ('TC 1 -  User succesfully Add item to cart', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    const dashboardPage = new DashboardPage(page)
+    const cartPage = new CartPage(page)
+
+    await loginPage.navigate()
+    await loginPage.login ('standard_user', 'secret_sauce')
+    await dashboardPage.validateOnPage ()
+    await cartPage.validateItemInCart ()
+
+});
